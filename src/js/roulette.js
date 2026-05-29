4c6213820f317a932cfbdf436cc43d6fbe7422d9
Atadz <Alissa.MAKSOUD@ortmontreuil.fr>
2026-05-29 12:58:17 +0200

    emoji et desactivé bouton lancer

--- src/js/roulette.js
+++ src/js/roulette.js
@@ -1,27 +1,24 @@
-const possibilité = ["Résultat 1 :7", "Résultat 2 : Diamant", "Résultat 3: Cerise "];
+const possibilité = ["7️⃣", "💎", "🍒","💰","🍀","🔔","🍋","🥐"];
 const boutonLevier = document.getElementById("boutonLevier");
 const messageStatut = document.getElementById("messageStatut");
 const AfficheResultat = document.getElementById("AfficheResultat");
 
-        function jouerRoulette() {
-            boutonLevier.disabled = true;
+        function jouerRoulette(e) {
+            e.disabled = true;
             messageStatut.textContent = "La roulette tourne...";
             AfficheResultat.textContent = "";
             setTimeout(() => {
                 const index1 = Math.floor(Math.random() * possibilité.length);
                 const index2 = Math.floor(Math.random() * possibilité.length);
                 const index3 = Math.floor(Math.random() * possibilité.length);
-
-
-                const resultatGagnant = possibilité[index1];
-                const resultatGagnant = possibilité[index2];
-                const resultatGagnant = possibilité[index3];
-
+                
+                const resultatGagnant = `${possibilité[index1]} ${possibilité[index2]} ${possibilité[index3]}`;
+                
                 messageStatut.textContent = "La roulette s'est arrêtée !";
                 AfficheResultat.textContent = resultatGagnant;
 
-                boutonLevier.disabled = false;
+                e.disabled = false;
 
             }, 2000);
         }
-        boutonLevier.addEventListener("click", jouerRoulette);
+        boutonLevier.addEventListener("click", e => jouerRoulette(e.target));
