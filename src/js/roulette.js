8784833373fd09dee1a49fb4dbe91c10d50436ea
Atadz <Alissa.MAKSOUD@ortmontreuil.fr>
2026-06-12 09:20:15 +0200

    Ajout de la probabilitée

--- src/js/roulette.js
+++ src/js/roulette.js
@@ -1,21 +1,47 @@
-const possibilité = ["7️⃣", "💎", "🍒","💰","🍀","🔔","🍋","🥐"];
+const possibilité = [
+            { symbole: "7️⃣", poids: 3 },
+            { symbole: "💎", poids: 5 },
+            { symbole: "💰", poids: 3 },
+            { symbole: "🍀", poids: 10 },
+            { symbole: "🔔", poids: 1 },
+            { symbole: "🍒", poids: 19 },
+            { symbole: "🍋", poids: 3 },
+            { symbole: "🥐", poids: 20 }];
 const boutonLevier = document.getElementById("boutonLevier");
 const messageStatut = document.getElementById("messageStatut");
 const AfficheResultat = document.getElementById("AfficheResultat");
 
+
+function tirerSymbole() {
+            const totalPoids = 64; // Total des poids du tableau
+            let rand = Math.floor(Math.random() * totalPoids);
+            let cumul = 0;
+            
+            for (let item of possibilité) {
+                cumul += item.poids;
+                if (rand < cumul) {
+                    return item.symbole;
+                }
+            }
+        }
         function jouerRoulette(e) {
             e.disabled = true;
             messageStatut.textContent = "La roulette tourne...";
             AfficheResultat.textContent = "";
             setTimeout(() => {
-                const index1 = Math.floor(Math.random() * possibilité.length);
-                const index2 = Math.floor(Math.random() * possibilité.length);
-                const index3 = Math.floor(Math.random() * possibilité.length);
+                const symbole1 = tirerSymbole();
+                const symbole2 = tirerSymbole();
+                const symbole3 = tirerSymbole();
                 
-                const resultatGagnant = `${possibilité[index1]} ${possibilité[index2]} ${possibilité[index3]}`;
+                const resultatGagnant = `${symbole1} ${symbole2} ${symbole3}`
                 
-                messageStatut.textContent = "La roulette s'est arrêtée !";
+                
                 AfficheResultat.textContent = resultatGagnant;
+                if (symbole1 === symbole2 && symbole2 === symbole3) {
+                    messageStatut.textContent = "La roulette s'est arrêtée ! Gagné !";
+                } else {
+                    messageStatut.textContent = "La roulette s'est arrêtée ! Perdu !";
+                }
 
                 e.disabled = false;
 
