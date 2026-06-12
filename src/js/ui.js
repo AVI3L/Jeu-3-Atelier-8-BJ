03883ae91d3c2e272cb4e8ddf08e210961614bab
4samsamAC <58746460+4samsamAC@users.noreply.github.com>
2026-06-12 10:36:35 +0200

    Rework CSS styles for memo page and card components

--- src/js/ui.js
+++ src/js/ui.js
@@ -6,9 +6,9 @@ function updateUI() {
     cards.forEach(card => {
         const div = document.createElement("div");
 
-        div.className = "card";
+        div.className = card.flipped || card.matched ? "card card-back half-flip" : "card card-back";
 
-        div.innerHTML = card.flipped || card.matched ? `<div class="card-content">${card.symbol}</div>` : `<div class="card-content">?</div>`;
+        div.innerHTML = card.flipped || card.matched ? `<div class="card-content">${card.symbol}</div>` : `<div class="card-content"></div>`;
 
         div.addEventListener("click", () => {
             flipCard(card.id);
