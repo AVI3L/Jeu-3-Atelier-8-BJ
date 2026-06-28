711d86f6767e5c8141e9f5e03dd65402b33a95e5
4samsamAC <58746460+4samsamAC@users.noreply.github.com>
2026-06-28 15:16:51 +0200

    Update game rendering logic to make animation work well.

--- src/js/game.js
+++ src/js/game.js
@@ -20,7 +20,7 @@ class Game {
 
         /** @type {boolean} */
         this.lockBoard = false;
-        
+
         /** @type {Deck} */
         this.deck = new Deck();
         this.deck.shuffle();
@@ -35,7 +35,7 @@ class Game {
 
         /** @type {Card[]} */
         const newCards = [];
-        
+
         this.cards.forEach((card, index) => newCards.push(new Card(card.getValue(), card.getSuit()).setId(index)));
 
         console.log(newCards);
@@ -47,34 +47,34 @@ class Game {
         this.cards = this.deck.cards;
 
         this.resetTurn();
-        
-        this.updateUI();
+
+        this.renderBoard();
     }
 
     checkMatch() {
         this.lockBoard = true;
-  
+
         const isMatch = this.firstCard.getSymbol() === this.secondCard.getSymbol();
-    
+
         if (isMatch) {
-        this.firstCard.matched = true;
-        this.secondCard.matched = true;
-    
-        this.resetTurn();
-    
-        this.checkWin();
-    
-        } else {
-    
-        setTimeout(() => {
-            this.firstCard.flipped = false;
-            this.secondCard.flipped = false;
-    
+            this.firstCard.matched = true;
+            this.secondCard.matched = true;
+
             this.resetTurn();
-    
-            this.updateUI();
-    
-        }, 1500);
+
+            this.checkWin();
+
+        } else {
+
+            setTimeout(() => {
+                this.firstCard.flipped = false;
+                this.secondCard.flipped = false;
+
+                this.resetTurn();
+
+                this.updateUI();
+
+            }, 1500);
         }
     }
 
@@ -86,58 +86,93 @@ class Game {
 
     checkWin() {
         this.won = this.cards.every(card => card.matched);
-    
+
         if (this.won) {
-        alert("Bravo !");
+            setTimeout(() => {
+                alert("Bravo !");
+            }, 1000);
         }
     }
 
     flipCard(cardId) {
         if (this.lockBoard) return;
-    
+
         const card = this.cards.find(c => c.getId() === cardId);
-    
+
         if (card.flipped || card.matched) return;
-    
+
         card.flip();
-    
-        this.updateUI();
-    
+
+        this.updateCard(card);
+
         if (!this.firstCard) {
-        this.firstCard = card;
-        return;
+            this.firstCard = card;
+            return;
         }
-    
+
         this.secondCard = card;
-    
+
         this.checkMatch();
     }
 
-    updateUI() {
+    renderBoard() {
         const frame = document.getElementById("game");
-
         frame.innerHTML = "";
 
         this.cards.forEach(card => {
-            console.log(card);
             const div = document.createElement("div");
 
+            div.className = "card card-back";
             div.dataset.cardId = card.getId();
-
-            div.className = card.flipped || card.matched ? "card card-back" : "card card-back";
 
             div.addEventListener("click", () => {
                 this.flipCard(card.getId());
             });
 
-            setTimeout(() => { div.className = card.flipped || card.matched ? "card card-back half-flip" : "card card-back flip";}, 1);
-
-            setTimeout(() => { div.className = card.flipped || card.matched ? "card card-front final-flip" : "card card-back flip"; 
-            div.innerHTML = card.flipped || card.matched ? `<div class="card-content final-flip ${card.getSuit() === '♥' || card.getSuit() === '♦' ? 'text-red' : 'text-black'}">${card.getSymbol()}</div>` : `<div class="card-content"></div>`;
-
-            }, 500);
-
             frame.appendChild(div);
         });
+
+        this.updateUI();
+    }
+
+    updateUI() {
+        this.cards.forEach(card => {
+            const div = document.querySelector(
+                `[data-card-id="${card.getId()}"]`
+            );
+
+            if (!div) return;
+
+            if (card.flipped || card.matched) {
+                div.className = "card card-front final-flip";
+                div.innerHTML =
+                    `<div class="card-content ${
+                    card.getSuit() === "♥" || card.getSuit() === "♦"
+                        ? "text-red"
+                        : "text-black"
+                }">${card.getSymbol()}</div>`;
+            } else {
+                div.className = "card card-back";
+                div.innerHTML = "";
+            }
+        });
+    }
+
+    updateCard(card) {
+        const div = document.querySelector(
+            `[data-card-id="${card.getId()}"]`
+        );
+
+        div.classList.add("half-flip");
+
+        setTimeout(() => {
+            div.className = "card card-front final-flip";
+            div.innerHTML =
+                `<div class="card-content ${
+                card.getSuit() === "♥" || card.getSuit() === "♦"
+                    ? "text-red"
+                    : "text-black"
+            }">${card.getSymbol()}</div>`;
+        }, 500);
     }
 }
